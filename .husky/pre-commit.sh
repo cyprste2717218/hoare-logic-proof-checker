62927882abe222090805b572e58574485df426b5
Stephen Ingham <smi505@york.ac.uk>
2025-03-17 13:28:40 +0000

    chore: testing

--- .husky/pre-commit.sh
+++
@@ -1,6 +0,0 @@
-echo "pre-commit hook started (husky)"
-echo "-------------------------------"
-echo "running xo and prettier checks "
-npm run lint
-echo "running npm packages audit"
-npm audit
--- .husky/pre-push.sh
+++
@@ -1,4 +0,0 @@
-echo "pre-push hook started (husky)"
-echo "-------------------------------"
-echo "validating web app builds succesfully"
-npm run build
