f672edbdf05cba23fccdd8b4a9ac604d2f529f58
Stephen Ingham <smi505@york.ac.uk>
2025-03-17 15:26:06 +0000

    chore: finished set up of xo and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-createRoot(document.querySelector('#root')).render(
+createRoot(document.querySelector('#root') as Element).render(
 	<StrictMode>
 		<App />
 	</StrictMode>,
