7bf0cf2b05bea7badd8e39f7b7eaae7fa05b5c55
Stephen Ingham <smi505@york.ac.uk>
2025-03-27 15:22:55 +0000

    chore: Proof Entry Input element now uses underlyign shadcn/text area aligned with auto-line numbering system for proof entry

--- src/components/proof-entry-input/proof-entry-input.tsx
+++ src/components/proof-entry-input/proof-entry-input.tsx
@@ -1,22 +1,16 @@
-import * as React from 'react';
-import {cn} from '@/lib/utils.js';
+import {useState} from 'react';
+import {TextArea} from './textarea';
 
-export function ProofEntryInput({
-	className,
-	type,
-	...properties
-}: React.ComponentProps<'input'>) {
+export function ProofEntryInput() {
+	const [value, setValue] = useState('');
 	return (
-		<input
-			type={type}
-			data-slot="input"
-			className={cn(
-				'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-				'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-				'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-				className,
-			)}
-			{...properties}
+		<TextArea
+			name="proof-entry-input-textarea"
+			value={value}
+			onValueChange={(value: string) => {
+				setValue(value);
+			}}
+			numOfLines={10}
 		/>
 	);
 }
---
+++ src/components/proof-entry-input/textarea.tsx
@@ -0,0 +1,135 @@
+import * as React from 'react';
+import {useMemo, useRef} from 'react';
+import {cn} from '@/lib/utils';
+
+type WrapperTextAreaProps = {
+	children: React.ReactNode;
+};
+
+type CustomTextAreaProps = {
+	handleTextAreaChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void; // eslint-disable-next-line @typescript-eslint/ban-types
+	textAreaRef: React.RefObject<HTMLTextAreaElement | null>;
+	handleTextAreaScroll: () => void;
+} & React.ComponentProps<'textarea'>;
+
+type TextAreaProps = {
+	value: string;
+	numOfLines: number;
+	onValueChange: (value: string) => void;
+	placeholder?: string;
+	name?: string;
+} & React.ComponentProps<'textarea'>;
+
+type LineNumbersProps = {
+	children: React.ReactNode;
+} & React.ComponentProps<'div'>;
+
+type LineNumberElemProps = {
+	count: number;
+};
+
+function TextArea({
+	className,
+	value,
+	numOfLines,
+	onValueChange,
+	placeholder = 'Enter Message',
+	name,
+	...props
+}: TextAreaProps) {
+	const lineCount = useMemo(() => value.split('\n').length, [value]);
+	const linesArr = useMemo(
+		() =>
+			Array.from({length: Math.max(numOfLines, lineCount)}, (_, i) => i + 1),
+		[lineCount, numOfLines],
+	);
+
+	const lineCounterRef = useRef<HTMLDivElement>(null);
+	const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+	const handleTextAreaChange = (
+		event: React.ChangeEvent<HTMLTextAreaElement>,
+	) => {
+		onValueChange(event.target.value);
+	};
+
+	const handleTextAreaScroll = () => {
+		if (lineCounterRef.current && textAreaRef.current) {
+			lineCounterRef.current.scrollTop = textAreaRef.current.scrollTop;
+		}
+	};
+
+	return (
+		<WrapperTextArea>
+			<LineNumbers ref={lineCounterRef}>
+				{linesArr.map((count) => (
+					<LineNumber key={count} count={count} />
+				))}
+			</LineNumbers>
+			<CustomTextArea
+				name={name}
+				handleTextAreaChange={handleTextAreaChange}
+				textAreaRef={textAreaRef}
+				handleTextAreaScroll={handleTextAreaScroll}
+				placeholder={placeholder}
+				value={value}
+				className={cn(className)}
+				{...props}
+			/>
+		</WrapperTextArea>
+	);
+}
+
+function WrapperTextArea({children}: WrapperTextAreaProps) {
+	return (
+		<div style={{display: 'flex', justifyContent: 'center'}}>{children}</div>
+	);
+}
+
+function LineNumbers({children}: LineNumbersProps) {
+	return (
+		<div className="py-2 px-2" style={{marginTop: '1px'}}>
+			{children}
+		</div>
+	);
+}
+
+function LineNumber({count}: LineNumberElemProps) {
+	return (
+		<div key={count}>
+			<p className="text-sm leading-7 [&:not(:first-child)]:mt-6">{count}</p>
+		</div>
+	);
+}
+
+function CustomTextArea({
+	name,
+	handleTextAreaChange,
+	textAreaRef,
+	handleTextAreaScroll,
+	placeholder,
+	value,
+	className,
+	...props
+}: CustomTextAreaProps) {
+	return (
+		<textarea
+			name={name}
+			onChange={handleTextAreaChange}
+			ref={textAreaRef}
+			onScroll={handleTextAreaScroll}
+			placeholder={placeholder}
+			value={value}
+			wrap="off"
+			data-slot="textarea"
+			className={cn(
+				'resize-none text-sm leading-7 border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+				className,
+			)}
+			style={{lineHeight: '28px', height: 'auto', minHeight: '28px'}}
+			{...props}
+		/>
+	);
+}
+
+export {TextArea};
