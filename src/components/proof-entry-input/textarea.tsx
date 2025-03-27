68d9de39cbea8a6235e4bf1a4b707f29698729d2
Stephen Ingham <smi505@york.ac.uk>
2025-03-27 15:48:05 +0000

    chore: altered UI to 'combine' line numbering closer with proof body entry

--- src/components/proof-entry-input/textarea.tsx
+++ src/components/proof-entry-input/textarea.tsx
@@ -88,7 +88,7 @@ function WrapperTextArea({children}: WrapperTextAreaProps) {
 
 function LineNumbers({children}: LineNumbersProps) {
 	return (
-		<div className="py-2 px-2 text-slate-400" style={{marginTop: '1px'}}>
+		<div className="py-2 px-2 text-slate-400 resize-none text-sm leading-7 border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 field-sizing-content rounded-l-md border border-r-0 bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
 			{children}
 		</div>
 	);
@@ -123,7 +123,7 @@ function CustomTextArea({
 			wrap="off"
 			data-slot="textarea"
 			className={cn(
-				'resize-none text-sm leading-7 border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+				'resize-none text-sm leading-7 border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content w-full rounded-r-md border border-l-0 bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
 				className,
 			)}
 			style={{lineHeight: '28px', height: 'auto', minHeight: '28px'}}
