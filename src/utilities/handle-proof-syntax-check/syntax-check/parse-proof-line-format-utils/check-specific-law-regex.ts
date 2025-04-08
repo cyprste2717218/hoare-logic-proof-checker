69d0b90d68d1c5e177f4bf9dc39ac9c2ef7ee9b5
Stephen Ingham <smi505@york.ac.uk>
2025-04-08 19:25:20 +0100

    chore: initial law check function now works as expected for stated law suffixes per relevant regex in validRegexFormats

--- src/utilities/handle-proof-syntax-check/syntax-check/parse-proof-line-format-utils/check-specific-law-regex.ts
+++ src/utilities/handle-proof-syntax-check/syntax-check/parse-proof-line-format-utils/check-specific-law-regex.ts
@@ -1,7 +1,6 @@
-import {validRegexFormats} from '@/lib/regex-formats';
 import {doLawSpecificChecks} from '@/utilities/handle-proof-syntax-check/syntax-check/parse-proof-line-format-utils/do-law-specific-checks';
 import {checkLawGroup} from '@/utilities/handle-proof-syntax-check/syntax-check/parse-proof-line-format';
-import type {LawTypeKeys, LawType, DiagnosticsType} from '@/models/misc';
+import type {LawTypeKeys, DiagnosticsType} from '@/models/misc';
 
 // Checks line adheres to spec for a proof line containing that law
 function checkSpecificLawRegex(
