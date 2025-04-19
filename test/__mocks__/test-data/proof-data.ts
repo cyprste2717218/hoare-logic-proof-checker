7901bc7a6caee4e59ba3a0a6e65b18d478ccedd5
Stephen Ingham <smi505@york.ac.uk>
2025-04-19 19:10:14 +0100

    chore: added initial unit tests for assignment law proof testing, currently set to skip pending hassign proof implementation support

--- test/__mocks__/test-data/proof-data.ts
+++ test/__mocks__/test-data/proof-data.ts
@@ -5,6 +5,11 @@ const validProofData = {
 		proofThree: ['x>4 /\\ y=2 /\\ z=3 -> x>4 /\\ y=2 /\\ z=3  :arith', '{x>4 /\\ y=2 /\\ z=3} x>4 /\\ y=2 /\\ z=3 {x>4 /\\ y=2 /\\ z=3} :hskip 1'],
 		proofFour: ['a<=6 /\\ b>3 /\\ c<=2 -> a<=6 /\\ b>3 /\\ c<=2 :arith', '{a<=6 /\\ b>3 /\\ c<=2} a<=6 /\\ b>3 /\\ c<=2 {a<=6 /\\ b>3 /\\ c<=2} :hskip 1']
 	},
+	assignLaw: {
+		proofOne: ['x=1 -> x+2=3 :arith', 'x=1 -> (x=3) [x|>x+2] :subst 1', '{x=1} x:=x+2 {x=3} :hassign 2'],
+		proofTwo: ['x>1 /\\ y>1 -> x+y>2 :arith', 'x>1 /\\ y>1 -> (x>2)[x|>x+y] :subst 1', '{x>1 /\\ y>1} x:=x+y {x>2} :hassign 2'],
+		proofThree: ['x=1 -> x+3>1 :arith', 'x=1 -> (x>1)[x|>x+3] :subst 1', '{x=1} x:=x+3 {x>1} :hassign 2']
+	}
 }
 
 const invalidProofData = {
@@ -13,6 +18,9 @@ const invalidProofData = {
 		proofTwo: ['a>1 /\\ b<3 -> b>1 /\\ a<3 :arith', '{a>1 /\\ b<3} a>1 /\\ b<3 {a>1 /\\ b<3} :hskip 1'],
 		proofThree: ['a<g -> a<g :arith', '{a<g} a<g {a<g} :hskip 1'],
 		proofFour: ['x=1 /\\ y>3 /\\ z=6 /\\ w=2 -> x=1 /\\ y>3 /\\ z=6 /\\ w=2  :arith', '{x=1 /\\ y>3 /\\ z=6 /\\ w=2} x=1 /\\ y>3 /\\ z=6 /\\ w=2  {x=1 /\\ y>3 /\\ z=6 /\\ w=2} :hskip 1']
+	},
+	assignLaw: {
+		proofOne: ['y>1 -> y>2 :arith', 'y>1 -> (y>2)[x|>x+1] :subst 1', '{y>1} x:=x+1 {y>2} :hassign 2']
 	}
 }
 
---
+++ test/unit/assign-law/invalid-proof.unit.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from '@jest/globals';
+import { handleCheckProofValidity } from "../../../src/utilities/proof-handle-utilities"
+import { invalidProofData } from '../../__mocks__/test-data/proof-data';
+
+
+describe.skip('Assign Law Invalid Proofs', () => {
+	test('should invalidate clearly false two variable proof, i.e. execution of program from precondition does not support evidence for postcondition', () => {
+
+		const inputData = invalidProofData.assignLaw.proofOne;
+		return handleCheckProofValidity(inputData).then(
+			(result: boolean) => { expect(result).toEqual(false) }
+		);
+
+	});
+
+})
